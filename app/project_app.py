752a8d771d96203927e75f659eaeef5d9d8f3ffc
CatalinToma511 <catalintoma511@gmail.com>
2025-07-23 16:28:31 +0300

    Steering fixed

--- app/project_app.py
+++ app/project_app.py
@@ -4,7 +4,26 @@ from ble_central import BLE_Central
 from car import Car
 
 def controls_callback(data):
-    print(data)
+    for b in data:
+        print(b, ' ')
+    global my_car
+    spd = data[0] - data[1]
+    if spd != 0:
+        my_car.led.on()
+    else:
+        my_car.led.off()
+
+    if spd >= 0:
+        my_car.motor.set_direction(1)
+    else:
+        my_car.motor.set_direction(-1)
+
+        # speed
+    my_car.motor.set_speed(int(abs(spd)/255 * 100))
+        
+    # steering
+    my_car.steering.set_steering_position(data[2]  - 127)
+
 
 async def main_task():
     ble = BLE_Central("PicoW_BLE")
--- app/steering.py
+++ app/steering.py
@@ -2,38 +2,24 @@ from servo import Servo
 
 
 class Steering:
-    def __init__(self, steering_servo_pin, center = 90, left = 45, right = 125, half_left = 67, half_right = 111):
+    def __init__(self, steering_servo_pin, center = 90, left = 45, right = 125):
         self.steering_servo = Servo(steering_servo_pin)
         self.center = center
         self.left = left
         self.right = right
-        self.half_left = half_left
-        self.half_right = half_right
         self.steering_servo.set_angle(self.center)
         self.steer_position = 0
-        
-        
-    def set_steering_position_old(self, pos):
-        print(pos)
-        if pos == -2:
-            self.steer_position = self.left
-        elif pos == -1:
-            self.steer_position = self.half_left
-        elif pos == 0:
-            self.steer_position = self.center
-        elif pos == 1:
-            self.steer_position = self.half_right
-        elif pos == 2:
-            self.steer_position = self.right
-        else:
-            print(f'[Steering]: Invalid position: {pos}')
-            return
-        self.steering_servo.set_angle(self.steer_position)
+
 
     def set_steering_position(self, pos):
         angle = 0
-        # right steering
+        # mapping
         if pos >=0:
-            angle = pos / 128 * (self.right - self.center)
+            angle = self.left + (self.center - self.left) / 128 * pos
         else:
-            angle = abs(pos) / 127 * (self.center - self.right)
+            angle = self.center + (self.right - self.center) / 127 * pos
+        # validating
+        if self.left <= angle <= self.right:
+            self.steering_servo.set_angle(angle)
+        else:
+            print(f'[Steering]: Invalid position: {pos}')
