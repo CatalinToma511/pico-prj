99f921703913696564f595cf1c17ab1958b9a502
CatalinToma511 <catalintoma511@gmail.com>
2025-07-31 15:36:22 +0300

    Changed horn to active buzzer

--- app/horn.py
+++ app/horn.py
@@ -3,12 +3,16 @@ from machine import Pin, PWM
 class Horn:
     def __init__(self, pin):
         self.horn_pin = Pin(pin, Pin.OUT)
-        self.horn_pwm = PWM(self.horn_pin)
-        self.horn_pwm.freq(500)
-        self.horn_pwm.duty_u16(0)
+        # self.horn_pwm = PWM(self.horn_pin)
+        # self.horn_pwm.freq(500)
+        # self.horn_pwm.duty_u16(0)
 
     def turn_on(self):
-        self.horn_pwm.duty_u16(65535 // 2)
+        # self.horn_pwm.duty_u16(65535 // 2)
+        self.horn_pin.on()
+        pass
 
     def turn_off(self):
-        self.horn_pwm.duty_u16(0)
+        # self.horn_pwm.duty_u16(0)
+        self.horn_pin.off()
+        pass
