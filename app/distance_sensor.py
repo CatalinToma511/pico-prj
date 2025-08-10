71c05bcdae287901bd87153480a5639e1d4f086b
CatalinToma511 <catalintoma511@gmail.com>
2025-08-10 12:21:05 +0300

    Added small delay to distance sensor init

--- app/distance_sensor.py
+++ app/distance_sensor.py
@@ -1,21 +1,27 @@
 from machine import I2C
 from vl53l0x import VL53L0X
+import time
 
 class DistanceSensor:
     def __init__(self, bus_id, scl_pin, sda_pin):
         i2c = I2C(id=bus_id, scl=scl_pin, sda=sda_pin)
         self.vl53l0x = VL53L0X(i2c)
+        time.sleep_ms(10)
         self.vl53l0x.set_measurement_timing_budget(250000)
+        time.sleep_ms(10)
         self.vl53l0x.set_Vcsel_pulse_period(self.vl53l0x.vcsel_period_type[0], 18)
+        time.sleep_ms(10)
         self.vl53l0x.set_Vcsel_pulse_period(self.vl53l0x.vcsel_period_type[1], 18)
+        time.sleep_ms(10)
         self.vl53l0x.start()
         self.distance_offset = -50
         self.old_distance = 0
+        time.sleep_ms(10)
     
     # Returns the distance in milimeters
     def read(self, low_pass_filter = True):
         distance = self.vl53l0x.read() + self.distance_offset #type: ignore
         if low_pass_filter is True:
-            distance = distance * 0.8 + self.distance_offset
+            distance = distance * 0.9 + self.old_distance * 0.1
         self.old_distance = distance
         return distance
