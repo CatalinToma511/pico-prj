ba530474b53dde03190ddc43150489855bcaf51c
CatalinToma511 <catalintoma511@gmail.com>
2025-08-14 00:51:50 +0300

    Modified voltage correction factor

--- app/voltagereader.py
+++ app/voltagereader.py
@@ -1,7 +1,7 @@
 from machine import ADC, Pin
 
 class VoltageReader:
-    def __init__(self, pin, R1 = 998_000, R2 = 470_000, correction_factor = 1.06):
+    def __init__(self, pin, R1 = 998_000, R2 = 470_000, correction_factor = 1.08):
         self.adc_pin = ADC(Pin(pin))
         self.R1 = R1
         self.R2 = R2
