ff210b926d9d8b538c035bee92f1b31af99c7c72
CatalinToma511 <catalintoma511@gmail.com>
2025-09-10 19:58:39 +0300

    Changed distance sensor param

--- app/distance_sensor.py
+++ app/distance_sensor.py
@@ -6,7 +6,7 @@ class DistanceSensor:
     def __init__(self, bus_id, scl_pin, sda_pin):
         i2c = I2C(id=bus_id, scl=scl_pin, sda=sda_pin)
         self.vl53l0x = VL53L0X(i2c)
-        self.vl53l0x.set_measurement_timing_budget(250000)
+        self.vl53l0x.set_measurement_timing_budget(20000)
         self.vl53l0x.set_Vcsel_pulse_period(self.vl53l0x.vcsel_period_type[0], 18)
         self.vl53l0x.set_Vcsel_pulse_period(self.vl53l0x.vcsel_period_type[1], 14)
         self.vl53l0x.start()
@@ -17,6 +17,6 @@ class DistanceSensor:
     def read(self, low_pass_filter = True):
         distance = self.vl53l0x.read() + self.distance_offset #type: ignore
         if low_pass_filter is True:
-            distance = distance * 0.9 + self.old_distance * 0.1
+            distance = distance * 0.7 + self.old_distance * 0.3
         self.old_distance = distance
         return distance
