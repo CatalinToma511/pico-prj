3ed3244045622b8288d953733fe3efa3d0b6354a
CatalinToma511 <catalintoma511@gmail.com>
2025-09-13 21:55:28 +0300

    telemetry and steering debug

--- app/steering.py
+++ app/steering.py
@@ -2,7 +2,7 @@ from servo import Servo
 
 
 class Steering:
-    def __init__(self, steering_servo_pin, center = 92., left = 135., right = 70.):
+    def __init__(self, steering_servo_pin, center = 88., left = 150., right = 30.):
         self.servo = Servo(steering_servo_pin)
         self.center = center
         self.left = left
