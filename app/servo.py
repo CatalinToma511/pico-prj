ae954b2314bf068652ff91a96f59f7194701dc1b
CatalinToma511 <catalintoma511@gmail.com>
2025-09-20 02:31:06 +0300

    Fixed servo library

--- app/servo.py
+++ app/servo.py
@@ -10,19 +10,14 @@ class Servo:
         self.pin = pin
         self.servo_pin = machine.PWM(machine.Pin(pin))
         self.servo_pin.freq(frequency)
-        self.write_microseconds(0)
+        self.servo_pin.duty_ns(0)
     
     
     def set_angle(self, angle):
         if 0 <= angle <= 180:
-            pulse_width = int(self.min_pulse_ms + (angle / 180) * (self.max_pulse_ms - self.min_pulse_ms))
-            self.write_microseconds(pulse_width)
+            pulse_width_ms = int(self.min_pulse_ms + (angle / 180) * (self.max_pulse_ms - self.min_pulse_ms))
+            self.servo_pin.duty_ns(pulse_width_ms * 1_000)
+            self.ms = pulse_width_ms
+            self.angle = int(angle)
         else:
             print(f'[Servo] Invalid angle: {angle}')
-    
-    
-    def write_microseconds(self, ms):
-        duty_cycle = int((ms / 20000) * 65535) # ms / (50hz = 20ms * 1000us) * max duty cycle (65535)
-        self.servo_pin.duty_u16(duty_cycle)
-        self.ms = ms
-        self.angle = (ms - self.min_pulse_ms) * 180 / (self.max_pulse_ms - self.min_pulse_ms)
