6acabb362fc7c0dd63dce49d2c480115addfeb8d
CatalinToma511 <catalintoma511@gmail.com>
2025-11-05 15:43:17 +0200

    Servo suspension config

--- app/project_app.py
+++ app/project_app.py
@@ -37,8 +37,8 @@ def run():
         my_car.config_voltage_reader(_VOLTAGE_PIN)
         my_car.config_mpu6050(_MPU_BUS_ID, _MPU_SCL_PIN, _MPU_SDA_PIN)
         my_car.config_distance_sensor(_VL53L0X_BUS_ID, _VL53L0X_SCL_PIN, _VL53L0X_SDA_PIN)
-        suspension_cfg = [('rl', _RL_SERVO_PIN, 90, 40, 100),
-                          ('rr', _RR_SERVO_PIN, 90, 150, 90)]
+        suspension_cfg = [('rl', _RL_SERVO_PIN, 90, 40, 120),
+                          ('rr', _RR_SERVO_PIN, 90, 150, 70)]
         my_car.config_suspension(suspension_cfg)
         ble = BLE_Server("PicoW_BLE", controls_callback=my_car.process_data)
         ble.advertise()
