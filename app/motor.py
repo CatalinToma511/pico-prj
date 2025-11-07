7a8883ff21eab33965669a6bbfadc49b82019152
CatalinToma511 <catalintoma511@gmail.com>
2025-11-07 04:22:57 +0200

    Changed dither high value and dither freq

--- app/motor.py
+++ app/motor.py
@@ -200,9 +200,9 @@ class Motor:
         self.pwm = 0
         self.dir_is_front = 1 # 1 = forward, 0 = backward
         self.dither_irq_pin = Pin(pwm_irq_pin)
-        self.pwm_dither_irq_pin = PWM(self.dither_irq_pin, freq = 1000)
+        self.pwm_dither_irq_pin = PWM(self.dither_irq_pin, freq = 500)
         self.dither_low = 500
-        self.dither_high = 65535 // 5
+        self.dither_high = 65535 // 4
         self.actual_pwm = 0
 
     def set_speed_limit_factor(self, speed_limit_factor):
