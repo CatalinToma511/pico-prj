1f7aa6f20f10d07dd58bd074184600242ea99cf7
CatalinToma511 <catalintoma511@gmail.com>
2026-03-20 22:23:17 +0200

    Changed modules config order (suspension before IMU)

--- app/project_app.py
+++ app/project_app.py
@@ -37,13 +37,13 @@ def run():
         my_car.config_gearbox(_GEARBOX_SHIFT_PIN)
         my_car.config_horn(_HORN_PIN)
         my_car.config_voltage_reader(_VOLTAGE_PIN)
-        my_car.config_mpu6050(_MPU_BUS_ID, _MPU_SCL_PIN, _MPU_SDA_PIN)
         my_car.config_distance_sensor(_VL53L0X_BUS_ID, _VL53L0X_SCL_PIN, _VL53L0X_SDA_PIN)
         suspension_cfg = [('fl', _FL_SERVO_PIN, 90, 130, 57),
                           ('fr', _FR_SERVO_PIN, 90, 47, 120),
                           ('rl', _RL_SERVO_PIN, 90, 56, 127),
                           ('rr', _RR_SERVO_PIN, 90, 117, 44)]
         my_car.config_suspension(suspension_cfg)
+        my_car.config_mpu6050(_MPU_BUS_ID, _MPU_SCL_PIN, _MPU_SDA_PIN)
         ble = BLE_Server("PicoW_BLE", controls_callback=my_car.process_data)
         ble.advertise()
 
