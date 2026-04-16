29abc34a7bb6ae8ce499a7ec456c76d00124d49a
CatalinToma511 <catalintoma511@gmail.com>
2026-04-16 16:16:36 +0300

    Added servo gradually position change

--- app/servo.py
+++ app/servo.py
@@ -1,24 +1,43 @@
-import machine
+from machine import Pin, PWM, Timer
 
 
 class Servo:
-    def __init__(self, pin, min_pulse_ms=500, max_pulse_ms=2500, frequency=50):
-        self.ms = 0
-        self.min_pulse_ms = min_pulse_ms
-        self.max_pulse_ms = max_pulse_ms
+    def __init__(self, pin, min_pulse_us=500, max_pulse_us=2500, frequency=50, deadzone = 0, speed_ms = 0, control_loop_interval_ms = 10):
+        self.pulse_width_target_us = 0
+        self.min_pulse_us = min_pulse_us
+        self.max_pulse_us = max_pulse_us
         self.angle = 0
+        self.deadzone = deadzone
         self.pin = pin
-        self.servo_pin = machine.PWM(machine.Pin(pin))
+        self.servo_pin = PWM(Pin(pin))
         self.servo_pin.freq(frequency)
         self.servo_pin.duty_ns(0)
+        self.max_step_us = self.max_pulse_us - self.min_pulse_us
+        self.control_loop_interval_ms = control_loop_interval_ms
+        self.control_loop_timer = Timer()
+        if speed_ms > 0:
+            # in servos, speed is often defined as the time it takes to move 60 degrees (666us), so we calculate the max step in microseconds based on that
+            # so, the max step is in us per control loop interval is 666 us divided by the speed in ms, multiplied by the control loop interval in ms
+            self.max_step_us = 666 / speed_ms * self.control_loop_interval_ms
+            self.control_loop_timer.init(period=self.control_loop_interval_ms, mode=Timer.PERIODIC, callback=self.control_loop)
+    
+
+    def control_loop(self, timer):
+        step_us = self.pulse_width_target_us - self.servo_pin.duty_ns() * 1000
+        step_us = max(min(step_us, self.max_step_us), -self.max_step_us)
+        pulse_width_ns = int(self.servo_pin.duty_ns() + step_us * 1000)
+        self.servo_pin.duty_ns(self.pulse_width_target_ns)
     
     
     def set_angle(self, angle):
-        if 0 <= angle <= 180:
-            angle = int(angle)
-            pulse_width_ms = int(self.min_pulse_ms + (angle / 180) * (self.max_pulse_ms - self.min_pulse_ms))
-            self.servo_pin.duty_ns(pulse_width_ms * 1_000)
-            self.ms = pulse_width_ms
-            self.angle = int(angle)
+        if abs(self.angle - angle) > self.deadzone and 0 <= angle <= 180:
+            pulse_width_us = self.min_pulse_us + (angle / 180) * (self.max_pulse_us - self.min_pulse_us)
+            pulse_width_us = max(min(pulse_width_us, self.max_pulse_us), self.min_pulse_us)
+            self.pulse_width_target_us = pulse_width_us
+            self.angle = angle
         else:
             print(f'[Servo] Invalid angle: {angle}')
+
+    
+    def deactivate(self):
+        self.servo_pin.duty_ns(0)
--- app/servocorner.py
+++ app/servocorner.py
@@ -2,17 +2,34 @@ from servo import Servo
 
 class ServoCorner:
     def __init__(self, servo_pin, center = 90, top_limit = 80, bottom_limit= 100):
-        self.servo = Servo(servo_pin)
+        self.servo = Servo(servo_pin, frequency=100, speed_ms=1000)
         self.center = center
         self.top_limit = top_limit
         self.bottom_limit = bottom_limit
-        self._set_angle(top_limit)
+        self.base_gain = 0
+        self.gain = 0
+        self._update()
 
     def _set_angle(self, angle):
-        if self.bottom_limit <= angle <= self.top_limit or self.top_limit <= angle <= self.bottom_limit:
-            self.servo.set_angle(angle)
+        if self.top_limit < self.bottom_limit:
+            angle = max(min(angle, self.bottom_limit), self.top_limit)
+        else:
+            angle = max(min(angle, self.top_limit), self.bottom_limit)
+        self.servo.set_angle(angle)
+
+    def _update(self):
+        self._set_angle(self.center + self.gain + self.base_gain)
 
     def set_gain(self, gain):
         if self.bottom_limit < self.top_limit:
             gain = -gain
-        self._set_angle(self.center + gain)
+        self.gain = gain
+        self._update()
+
+    def set_base_gain(self, gain):
+        if self.bottom_limit < self.top_limit:
+            gain = -gain
+        self.base_gain = gain
+        self._update()
+
+    
