74b7d79c547a1c2ed9a1001d790506c26a8ee055
CatalinToma511 <catalintoma511@gmail.com>
2026-04-16 17:07:53 +0300

    Added deadzone for steering input

--- app/steering.py
+++ app/steering.py
@@ -3,7 +3,7 @@ from machine import Timer
 
 
 class Steering:
-    def __init__(self, steering_servo_pin, center = 90., left = 135., right = 45., max_left_pos=-128, max_right_pos=127, center_pos=0, filter_alpha=0.4):
+    def __init__(self, steering_servo_pin, center = 90., left = 135., right = 45., max_left_pos=-128, max_right_pos=127, center_pos=0, pos_deadzone = 10, filter_alpha=0.4):
         self.servo = Servo(steering_servo_pin, frequency=200)
         self.center = center
         self.left = left
@@ -11,6 +11,13 @@ class Steering:
         self.max_left_pos = max_left_pos
         self.max_right_pos = max_right_pos
         self.center_pos = center_pos
+        self.pos_deadzone = pos_deadzone
+        if self.max_left_pos < self.max_right_pos:
+            self.min_left_pos = self.center_pos - self.pos_deadzone
+            self.min_right_pos = self.center_pos + self.pos_deadzone
+        else:
+            self.min_left_pos = self.center_pos + self.pos_deadzone
+            self.min_right_pos = self.center_pos - self.pos_deadzone
         self.filter_alpha = filter_alpha
         self.target_angle = center
         self.position = 0
@@ -31,11 +38,15 @@ class Steering:
 
     def set_steering_position(self, target_position):
         self.target_angle = self.center # just initialize to avoid unbound variable error
+        if abs(target_position - self.center_pos) < self.pos_deadzone:
+            self.target_angle = self.center
+            self.position = 0
+            return
         # mapping
         if self.max_left_pos <= target_position <= self.center_pos:
             # steering left
-            self.target_angle = self.center - abs(self.center - self.left) / abs(self.max_left_pos) * (target_position - self.center_pos)
+            self.target_angle = self.center - abs(self.min_left_pos - self.left) / abs(self.max_left_pos) * (target_position - self.center_pos)
         elif self.center_pos < target_position <= self.max_right_pos:
             # steering right
-            self.target_angle = self.center - abs(self.center - self.right) / abs(self.max_right_pos) * (target_position - self.center_pos)
+            self.target_angle = self.center - abs(self.min_right_pos - self.right) / abs(self.max_right_pos) * (target_position - self.center_pos)
         self.position = target_position
