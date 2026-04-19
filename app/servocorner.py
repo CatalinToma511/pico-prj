be77d0f34e8187121128a6f075c4d49c5b22a3f6
CatalinToma511 <catalintoma511@gmail.com>
2026-04-19 20:12:57 +0300

    Removed center parameter

--- app/servocorner.py
+++ app/servocorner.py
@@ -1,11 +1,12 @@
 from servo import Servo
 
 class ServoCorner:
-    def __init__(self, servo_pin, center = 90, top_limit = 80, bottom_limit= 100):
+    def __init__(self, servo_pin, top_limit = 80, bottom_limit= 100):
         self.servo = Servo(servo_pin, frequency=100, speed_ms=5000, control_loop_interval_ms=5)
-        self.center = center
         self.top_limit = top_limit
         self.bottom_limit = bottom_limit
+        self.total_travel = abs(bottom_limit - top_limit)
+        self.travel_coef = bottom_limit - top_limit
         self.base_gain = 0
         self.gain = 0
         self._update()
@@ -18,18 +19,19 @@ class ServoCorner:
         self.servo.set_angle(angle)
 
     def _update(self):
-        self._set_angle(self.center + self.gain + self.base_gain)
+        set_point = self.top_limit + self.gain + self.base_gain
+        self._set_angle(set_point)
 
-    def set_gain(self, gain):
-        if self.bottom_limit < self.top_limit:
-            gain = -gain
-        self.gain = gain
+    def set_gain(self, gain_norm):
+        self.gain = max(min(gain_norm, 1.), -1.) * self.travel_coef
         self._update()
 
-    def set_base_gain(self, gain):
-        if self.bottom_limit < self.top_limit:
-            gain = -gain
-        self.base_gain = gain
+    def set_base_gain(self, base_gain_norm):
+        self.base_gain = max(min(base_gain_norm, 1.), -1.) * self.travel_coef
         self._update()
 
-    
+    def add_gain(self, gain_norm):
+        self.gain += max(min(gain_norm, 1.), -1.) * self.travel_coef
+        self._update()
+    def get_total_gain(self):
+        return self.base_gain + self.gain
