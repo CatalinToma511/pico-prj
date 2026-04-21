359df216b9b799cb8345b1b90fc7fa8cd55bd12c
CatalinToma511 <catalintoma511@gmail.com>
2026-04-22 01:21:40 +0300

    Servo suspension endpoints changed

--- app/project_app.py
+++ app/project_app.py
@@ -38,9 +38,9 @@ def run():
         my_car.config_voltage_reader(_VOLTAGE_PIN)
         my_car.config_distance_sensor(_VL53L0X_BUS_ID, _VL53L0X_SCL_PIN, _VL53L0X_SDA_PIN)
         suspension_cfg = [('fl', _FL_SERVO_PIN, 137, 137, 72),
-                          ('fr', _FR_SERVO_PIN, 43, 43, 108),
+                          ('fr', _FR_SERVO_PIN, 45, 45, 111),
                           ('rl', _RL_SERVO_PIN, 42, 42, 117),
-                          ('rr', _RR_SERVO_PIN, 133, 133, 58)]
+                          ('rr', _RR_SERVO_PIN, 132, 132, 57)]
         my_car.config_suspension(suspension_cfg)
         my_car.config_mpu6050(_MPU_BUS_ID, _MPU_SCL_PIN, _MPU_SDA_PIN)
         ble = BLE_Server("PicoW_BLE", controls_callback=my_car.process_data)
