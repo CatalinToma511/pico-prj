edc8b659699f4618467bfd900d06fd1cf456e37d
CatalinToma511 <catalintoma511@gmail.com>
2026-06-19 01:45:48 +0300

    Changed suspension parameters

--- app/suspension.py
+++ app/suspension.py
@@ -28,10 +28,10 @@ class Suspension:
         self.mode = 0
         self.roll = 0
         self.pitch = 0
-        self.tilt_alpha = 0.9
+        self.tilt_alpha = 0.5
         self.incline_epsilon = 0.25 # degrees of acceptable incline, above this suspension tries to compensate
         # suspension can change roll by -13 to +13 deg, pitch by -5 to +5, and gain is from 0.0 to 1.0
-        self.kp_roll = 0.01
+        self.kp_roll = 0.075
         self.kp_pitch = 0.005
         self.diag_weight = 0
         self.diag_weight_exponent = 0.5
