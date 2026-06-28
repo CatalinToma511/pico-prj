44f097213b581cdfb63e84c593372983a8740067
CatalinToma511 <catalintoma511@gmail.com>
2026-06-28 03:17:53 +0300

    Adjusted servo corners speeds

--- app/project_app.py
+++ app/project_app.py
@@ -39,8 +39,8 @@ def run():
         # my_car.config_distance_sensor(_VL53L0X_BUS_ID, _VL53L0X_SCL_PIN, _VL53L0X_SDA_PIN)
         suspension_cfg = [('fl', _FL_SERVO_PIN, 45, 111, 750),
                           ('fr', _FR_SERVO_PIN, 137, 72, 750),
-                          ('rl', _RL_SERVO_PIN, 42, 117, 852),
-                          ('rr', _RR_SERVO_PIN, 132, 57, 852)]
+                          ('rl', _RL_SERVO_PIN, 42, 117, 660),
+                          ('rr', _RR_SERVO_PIN, 132, 57, 660)]
         my_car.config_mpu6050(_MPU_BUS_ID, _MPU_SCL_PIN, _MPU_SDA_PIN)
         my_car.config_suspension(suspension_cfg)
         ble = BLE_Server("PicoW_BLE", controls_callback=my_car.process_data)
